5ccb0df5a64dfdd8671a400507da7cdf6568d344
Emir Hasanbegovic <emir@emirweb.com>
2014-06-27 10:12:58 -0700

    @emir-hasanbegovic adding support library for Robolectric

--- library/src/test/java/mobi/parchment/widget/adapterview/AbstractAdapterViewManagerTest.java
+++ library/src/test/java/mobi/parchment/widget/adapterview/AbstractAdapterViewManagerTest.java
@@ -13,6 +13,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
 import mobi.parchment.R;
 
